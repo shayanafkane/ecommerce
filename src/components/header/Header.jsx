21bf3524e187edc02978d87504537637ea949d41
shayanafkane <shayanafkane@gmail.com>
2023-02-25 18:05:08 +0330

    update

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import Ekala from '../../assets/images/logo.svg'
+import './header.css'
+import { AiOutlineSearch } from 'react-icons/ai'
 function Header() {
-  return (
-    <div>Header</div>
-  )
+    return (
+        <div>
+            <div className="header">
+                <div className="scontainer flex">
+                    <div className="logo">
+                        <img height={30} src={Ekala} alt="" />
+                    </div>
+                    <div className="search flex">
+                        <AiOutlineSearch />
+                        <input type="text" placeholder='Search....' />
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Header
--- src/components/header/pages/home/Home.jsx
+++ src/components/header/pages/home/Home.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 
 function Home() {
-  return (
-    <div>Home</div>
-  )
+    window.addEventListener("scroll", function () {
+        const header = this.document.querySelector(".header")
+        header.classList.toggle("active", this.window.screenY > 100);
+    })
+    window.scrollTo({ top: document.body.scrollHeight, behavior: "smooth" })
+    return (
+        <div>
+            
+        </div>
+    )
 }
 
 export default Home
