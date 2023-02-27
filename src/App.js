d27b555f1ecb676be3e6626353484afd6bf04edb
shayanafkane <shayanafkane@gmail.com>
2023-02-27 16:38:10 +0330

    update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-
-import React from 'react'
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import Header from "./components/header/Header";
-import Home from "./components/header/pages/home/Home";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-    <>
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/" component={Home} />
-        </Switch>
-      </Router>
-    </>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
