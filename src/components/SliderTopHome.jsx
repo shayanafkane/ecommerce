02f441bd1523051289af1d42f615c9df28e7ae44
shayanafkane <shayanafkane@gmail.com>
2023-02-28 15:52:39 +0330

    update swiper

--- src/components/SliderTopHome.jsx
+++ src/components/SliderTopHome.jsx
@@ -5,15 +5,16 @@ import { Swiper, SwiperSlide } from "swiper/react";
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/navigation";
+import "swiper/css/autoplay"
 
 
 // import required modules
-import { Navigation } from "swiper";
+import { Autoplay, Navigation } from "swiper";
 function SliderTopHome() {
     return (
         <div className='mt-12 '>
             <div className="box-slider-top-home container mx-auto bg-slate-400 h-96 rounded-2xl">
-                <Swiper dir="rtl" navigation={true} modules={[Navigation]} className="mySwiper h-full w-full" >
+                <Swiper autoplay={true} dir="rtl" navigation={true} modules={[Navigation , Autoplay]} className="mySwiper h-full w-full" >
                     <SwiperSlide >
                         <div className="h-full rounded-2xl">
                             <img className="rounded-2xl" src="https://api.lorem.space/image/watch?w=1366&h=400&23" alt="" />
